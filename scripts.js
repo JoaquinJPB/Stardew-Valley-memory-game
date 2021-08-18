37fca16bbfc9638c18371cdbcbdee5547a452b52
Joaquin Javier Pastore Barrios <joaquin@MacBook-Pro-de-Joaquin.local>
2021-08-18 17:25:23 +0100

    +20 Cards

---
+++ scripts.js
@@ -0,0 +1,11 @@
+
+function start_game(){
+
+    let div_images = document.createElement("div");
+
+
+    document.getElementById("button-start").style.display = 'none';
+    document.getElementById("description").style.display = 'none';
+
+    document.getElementById("div-game").appendChild(div_images);
+}
