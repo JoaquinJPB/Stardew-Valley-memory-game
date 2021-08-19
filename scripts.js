088f459b6b31396373fc3c6e8e16c787b7923328
Joaquin Javier Pastore Barrios <joaquin@MacBook-Pro-de-Joaquin.local>
2021-08-19 11:43:05 +0100

    Match cards

--- scripts.js
+++ scripts.js
@@ -1,8 +1,28 @@
 
 const cards = document.querySelectorAll('.memory-card');
 
+let firstCard, secondCard;
+let hasFlippedCard = false;
+
 function flipCard(){
     this.classList.toggle('flip');
+    if(!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;
+    } else {
+        hasFlippedCard = false;
+        secondCard = this;
+        if (firstCard.dataset.stardew === secondCard.dataset.stardew) {
+            firstCard.removeEventListener('click', flipCard);
+            secondCard.removeEventListener('click', flipCard);
+        } else {
+            setTimeout( () => {
+                firstCard.classList.remove('flip');
+                secondCard.classList.remove('flip');
+            }, 1000);
+        }
+        
+    }
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
