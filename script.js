0347b6d1d8b522b37fb68bb7d9716cc129255247
Joaquin Javier Pastore Barrios <joaquin@MacBook-Pro-de-Joaquin.local>
2021-09-01 16:29:06 +0100

    Muted button

--- script.js
+++ script.js
@@ -2,20 +2,29 @@
 const cards = document.querySelectorAll('.memory-card');
 cards.forEach(card => card.addEventListener('click', flipCard));
 
+const mutedButton = document.getElementById('muted');
+mutedButton.addEventListener('click', muteSound);
+
 let firstCard, secondCard;
 let lockBoard = false;
 let hasFlippedCard = false;
+let muted = false;
 let score = 0;
-const flipCardSound = new Audio("audio/card-flip-sound-effect.mp3");
-const rightPair = new Audio("audio/correct-answer-sound-effect.mp3");
-const wrongPair = new Audio("audio/wrong-pair.mp3");
-const winSound = new Audio("audio/stardew_valley_town.mp3");
+
+const audio = {
+    flipCardSound : new Audio("audio/card-flip-sound-effect.mp3"),
+    rightPair : new Audio("audio/correct-answer-sound-effect.mp3"),
+    wrongPair : new Audio("audio/wrong-pair.mp3"),
+    winSound : new Audio("audio/stardew_valley_town.mp3")
+    };
 
 function flipCard(){
     if (lockBoard) return;
     if (this === firstCard) return;
     this.classList.toggle('flip');
-    flipCardSound.play();
+    if (muted == false){
+        audio.flipCardSound.play();
+    }
     if(!hasFlippedCard) {
         hasFlippedCard = true;
         firstCard = this;
@@ -31,7 +40,9 @@ function checkMatch(){
 }
 
 function disableFlipCards(){
-    rightPair.play();
+    if (muted == false){
+        audio.rightPair.play();
+    }
     firstCard.removeEventListener('click', flipCard);
     secondCard.removeEventListener('click', flipCard);
     score++;
@@ -47,7 +58,9 @@ function resetBoard(){
 function unflipCards() {
     lockBoard = true;
     setTimeout( () => {
-        wrongPair.play();
+        if (muted == false){
+            audio.wrongPair.play();
+        }
         firstCard.classList.remove('flip');
         secondCard.classList.remove('flip');
         resetBoard();
@@ -62,8 +75,12 @@ function winGame(){
             icon: 'success',
             iconColor: '#2fb946'
         });
-        setTimeout( () => {winSound.play()},1000);
+        setTimeout( () => {audio.winSound.play()},1000);
     }
+}
+
+function muteSound() {
+    muted = true;
 }
 
 (function shuffle(){
